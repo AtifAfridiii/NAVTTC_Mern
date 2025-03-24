fa75cbea58ab7601fea184ff3efba674aed78aa1
Atif Afridi <atifafridi378@gmail.com>
2025-03-24 14:33:04 +0500

    javascript added

---
+++ Html_Css/Function/script.js
@@ -0,0 +1,12 @@
+console.log('Hello world 😶‍🌫️')
+
+console.log('My variable : ' + myvar);
+var myvar = 20;
+
+console.log('let : ' + mylet);
+let mylet = 30;
+
+console.log('constante : ' + myconst);
+const myconst = 10;
+
+
