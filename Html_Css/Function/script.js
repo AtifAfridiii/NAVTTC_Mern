7718621fe5f655c1b092d4ff54e279b50aa4ef4b
Atif Afridi <atifafridi378@gmail.com>
2025-03-25 05:01:21 +0500

    Task completed

--- Html_Css/Function/script.js
+++ Html_Css/Function/script.js
@@ -1,12 +1,32 @@
 console.log('Hello world 😶‍🌫️')
 
-console.log('My variable : ' + myvar);
-var myvar = 20;
+// console.log('My variable : ' + myvar);
+// var myvar = 20;     //  => undefined
 
-console.log('let : ' + mylet);
-let mylet = 30;
+// console.log('let : ' + mylet);
+// let mylet = 30;      // => error can not access before initailizing
 
-console.log('constante : ' + myconst);
-const myconst = 10;
+// console.log('constante : ' + myconst);
+// const myconst = 10;  //  => error can not access before initailizing
 
+// console.log(' ***********       ********    ***********     ************    ');
+// console.log(' ***********          **           **          **            ');
+// console.log(' **       **          **           **          **            ');
+// console.log(' **       **          **           **          **********    ');
+// console.log(' ***********          **           **          **            ');
+// console.log(' ***********          **           **          **            ');
+// console.log(' **       **          **           **          **            ');
+// console.log(' **       **          **           **          **            ');
+// console.log(' **       **          **       ***********     **            ');
+
+
+console.log('         **         ***********  ***********     **********    ');
+console.log('       **  **           **           **          **            ');
+console.log('      **     **         **           **          **            ');
+console.log('     **       **        **           **          **********    ');
+console.log('    ** ******* **       **           **          **            ');
+console.log('   **           **      **           **          **            ');
+console.log('  **             **     **           **          **            ');
+console.log(' **               **    **           **          **            ');
+console.log('**                 **   **       ***********     **            ');
 
