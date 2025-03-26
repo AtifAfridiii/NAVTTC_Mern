79cc928f1eb750bb2b7536240fb3810425e04664
Atif Afridi <atifafridi378@gmail.com>
2025-03-26 16:05:58 +0500

    New tasks done

---
+++ JavaScript/Functionality/Task2.js
@@ -0,0 +1,24 @@
+console.log(5+'5');
+
+let num = '10' ;
+console.log(typeof num);
+let Changed = Number(num) ;
+console.log(typeof Changed);
+
+console.log(true&&false);
+
+let Value1 = 5 ;
+let Value2 = 9 ;
+
+console.log(`Before Swap Value1 : ${Value1}`);
+console.log(`Before Swap Value2 : ${Value2}`);
+
+let Temp = 0 ;
+Temp = Value1 ;
+Value1 = Value2 ;
+Value2 = Temp ;
+
+console.log(`After Swap Value1 : ${Value1}`);
+console.log(`After Swap Value1 : ${Value2}`);
+console.log(false&&true||true);
+
