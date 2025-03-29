1ca2ef1a57d1425b3e7957209627ee7f6dc5fb76
Atif Afridi <atifafridi378@gmail.com>
2025-03-30 02:51:48 +0500

    Guess number Game

---
+++ JavaScript/Functionality/Loop_Task.js
@@ -0,0 +1,17 @@
+
+let Guess_Number =Number( (Math.floor(Math.random()*10)+1)) ;
+console.log(Guess_Number);
+
+let User_input = Number(prompt("Enter number to guess "));
+
+while(true){
+    if(User_input === Guess_Number){
+        console.log(`You guessed it ${Guess_Number}`);
+        break ;
+    }else if(User_input!==Guess_Number){
+        console.log(`Try again ${Guess_Number} `);
+        User_input = Number(prompt("Enter number to guess "));
+    }else{
+        console.log("Invalid input");
+    }
+}
