63d6dd88b0554b91029d14e6e24ac50245f2d0fa
Atif Afridi <atifafridi378@gmail.com>
2025-03-30 02:06:13 +0500

    Class tasks done

--- JavaScript/Functionality/Class_Task.js
+++ JavaScript/Functionality/Class_Task.js
@@ -1,12 +1,12 @@
-// let Prime_Number =  Number(prompt('Enter number'));
+let Prime_Number =  Number(prompt('Enter number'));
 
-// if( Prime_Number%2!=0 && Prime_Number%3!=0 ){
-//     console.log('It is Prime Number');
+if( Prime_Number%2!=0 && Prime_Number%3!=0 ){
+    console.log('It is Prime Number');
 
-// }else{
-//     console.log('Not a prime number');
+}else{
+    console.log('Not a prime number');
 
-// }
+}
 
 
 let Name = prompt('Enter your Name');
@@ -21,4 +21,14 @@ if (Name.toLowerCase()=='guest' && Passoward.toLocaleLowerCase()=='guest123'){
 }else{
     console.log('Invalid');
 
+}
+
+let Check_Number = Number(prompt("Enter any number "));
+(Check_Number >= 10 && Check_Number <= 50) ? console.log(`The Number ${Check_Number} is in the range of 10-50`) : console.log('Not in range');
+
+let Alphabetic_Character = String(prompt('Enter any  single character'));
+if (Alphabetic_Character.length == 1 && Alphabetic_Character.toLocaleLowerCase() === 'a' || Alphabetic_Character.toLocaleLowerCase() === 'e' || Alphabetic_Character.toLocaleLowerCase() === 'i' || Alphabetic_Character.toLocaleLowerCase() === 'o' || Alphabetic_Character.toLocaleLowerCase() === 'u'){
+    console.log('Vowel');
+}else{
+    console.log('Consonant');
 }
