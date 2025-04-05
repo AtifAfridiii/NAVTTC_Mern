830ce502330af0086a5bc9da584ebd87c75a8d0a
Atif Afridi <atifafridi378@gmail.com>
2025-04-05 23:28:10 +0500

    Home tasks

--- JavaScript/Home_Tasks/Home_Task01.js
+++ JavaScript/Home_Tasks/Home_Task01.js
@@ -32,3 +32,24 @@ while (num > 0) {
 
 }
 console.log(`Number of Digits : ${count}`);
+
+
+/// Write a program to print numbers from 10 to 1 using a for loop (reverse order).
+
+for (let index = 10; index >=1 ; index-- ) {
+   console.log(`Number : ${index}`);
+
+}
+
+// Write a script that asks the user to input numbers until the sum of all entered numbers is greater than 100.
+
+let UserInput;
+let sum = 0;
+while(sum<=100){
+  UserInput = Number(prompt("Enter a number: "));
+  sum += UserInput;
+  console.log(`Current Sum : ${sum}`);
+}
+console.log(`Sum of numbers : ${sum}`);
+
+
