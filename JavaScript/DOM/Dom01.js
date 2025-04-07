ecd1f58de9b67aa17d8057d053d70ad4b7780c73
Atif Afridi <atifafridi378@gmail.com>
2025-04-07 22:42:54 +0500

    Dom Task completed

--- JavaScript/DOM/Dom01.js
+++ JavaScript/DOM/Dom01.js
@@ -27,12 +27,13 @@ for (let index = 0; index < mern.length ; index++) {
 
 }
 
-let newADD = document.getElementsByClassName("Note");
-newADD.className='Highlight';
-console.log(newADD[0].className);
+ document.getElementsByClassName('Note')[0].className = 'highlight';;
+document.getElementsByClassName('highlight')[0].style.backgroundColor='yellow';
+
+// document.getElementById('note1').className = 'highlight';
 
 // newADD[0].classList.add('Highlight');
-// console.log(newADD[0].classList);
+
 
 // console.log(newADD[0].classList.contains('Highlight'));
 // let new2= document.getElementsByClassName('Highlight')
@@ -45,5 +46,28 @@ console.log(List.length);
 for (let index = 0; index < List.length; index++) {
    console.log(` ${index} : ${List[index].innerText}`);
 
+
+}
+
+let First_Heading = document.getElementById('heading')
+console.log(First_Heading.innerText);
+First_Heading.style.backgroundColor='teal'
+
+
+// document.getElementsByTagName('div')[0].innerText = 'Hello World in div 1';
+// document.getElementsByTagName('div')[1].innerText = 'Hello World in div 2';
+// document.getElementsByTagName('div')[2].innerText = 'Hello World in div 3';
+
+let Div_Tags = document.getElementsByTagName('div')
+
+
+for (let index = 0; index < Div_Tags.length; index++) {
+   if(Div_Tags[index]===0){
+      Div_Tags[index].innerText = 'Hello World in div 1';
+   }else if(Div_Tags[index]===1){
+      Div_Tags[index].innerText = 'Hello World in div 2';
+   }else{
+      Div_Tags[index].innerText = 'Hello World in div 3';
+   }
 
 }
