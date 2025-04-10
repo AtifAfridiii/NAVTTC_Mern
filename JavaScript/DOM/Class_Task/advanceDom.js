48a389eee8a3fe4ca9087228cd2dc3dae482ea9b
Atif Afridi <atifafridi378@gmail.com>
2025-04-10 17:02:42 +0500

    JS list show and hide

--- JavaScript/DOM/Class_Task/advanceDom.js
+++ JavaScript/DOM/Class_Task/advanceDom.js
@@ -1,27 +1,51 @@
 
 
 let Parent = document.querySelector('.Parent');
-let Child = document.querySelectorAll('.Child');
 
 let ReplaceBtn = document.querySelector('.btn');
+let isCHange = false ;
 
 ReplaceBtn.addEventListener('click',function(event){
     event.preventDefault();
+    let Child = document.querySelectorAll('.Child');
+
    for (let index = 0; index < Child.length; index++) {
     let newChild = document.createElement('li');
-     if(index===0){
-        newChild.innerText='This is Replace Content';
-     }else if (index===1){
-        newChild.innerText='This is new Text at 1 index';
-     }else if(index===2){
-        newChild.innerText='This is a simple  Content';
-     }else if(index===3){
-        newChild.innerText='This is some Content';
-     }else{
-        newChild.innerText='This is some Content when the button is clicked ';
-     }
+    newChild.className = 'Child';
+    if(isCHange){
+        if(index===0){
+            newChild.innerText='This is Replace Content';
+         }else if (index===1){
+            newChild.innerText='This is new Text at 1 index';
+         }else if(index===2){
+            newChild.innerText='This is a simple  Content';
+         }else if(index===3){
+            newChild.innerText='This is some Content';
+         }else{
+            newChild.innerText='This is some Content when the button is clicked ';
+         }
+    }else{
+        if(index==0){
+            newChild.innerText='List 1'
+         } else if (index===1){
+            newChild.innerText='List 2';
+         }else if(index===2){
+            newChild.innerText='List 3';
+         }else if(index===3){
+            newChild.innerText='List 4';
+         }else{
+            newChild.innerText='List 5 ';
+         }
+    }
+
     Parent.replaceChild(newChild,Child[index]);
 
+
    }
+
+   isCHange=!isCHange;
 })
+
+
+
 
---
+++ JavaScript/DOM/Home_Task/todoList.js
@@ -0,0 +1,66 @@
+let userInput = document.querySelector('#todoInput');
+let btnInput = document.querySelector('#todoBtn');
+let ulParent = document.querySelector('#ulParent');
+emptyList = document.querySelector('#btnDiv');
+let noItem = document.querySelector('#noItemPara');
+
+btnInput.addEventListener('click', function (e) {
+    e.preventDefault();
+    let input = userInput.value.trim();
+
+    if (!input) {
+        alert('Please enter a task');
+
+
+        noItem.innerText='No task added yet';
+
+        emptyList.appendChild(noItem);
+        
+        return;
+    }
+
+    let taskWrapper = document.createElement('div');
+    taskWrapper.classList.add('flex', 'justify-between', 'items-center');
+
+
+    let newLi = document.createElement('li');
+    newLi.innerText = input;
+    newLi.classList.add(
+        'bg-gray-100',
+        'w-full',
+        'rounded-lg',
+        'px-4',
+        'py-2',
+        'text-gray-700',
+        'shadow-sm',
+        'cursor-pointer',
+        'transition-all',
+        'mr-2'
+    );
+
+
+    newLi.addEventListener('click', function () {
+        newLi.classList.toggle('line-through');
+        newLi.classList.toggle('text-gray-400');
+        newLi.classList.toggle('bg-green-500');
+    });
+
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'X';
+    deleteBtn.classList.add('text-red-500', 'font-bold', 'px-3', 'py-2');
+
+    deleteBtn.addEventListener('click', function () {
+        taskWrapper.remove();
+    });
+
+
+    taskWrapper.appendChild(newLi);
+    taskWrapper.appendChild(deleteBtn);
+
+
+    ulParent.appendChild(taskWrapper);
+
+
+    userInput.value = '';
+});
